b23a2bf844da32ba6ad2678640612fef4b0cac75
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-26 23:34:36 +0100

    Complete initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </div>
+  );
+}
+export default App;
