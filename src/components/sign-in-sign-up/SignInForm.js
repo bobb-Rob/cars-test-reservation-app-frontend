351b94f95c36446b52c258adc8ff4eff2519d637
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-27 13:45:50 +0100

    Add sign in page

--- src/components/sign-in-sign-up/SignInForm.js
+++ src/components/sign-in-sign-up/SignInForm.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { HiOutlineMail } from 'react-icons/hi';
 
 const SignInForm = () => (
-  <div>SignInForm</div>
+  <div>
+    <h2>Login</h2>
+    <span>Please sign in to continue</span>
+    <form action="#">
+      <div className="form-group">
+        <div>
+          <HiOutlineMail />
+          { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+          <label htmlFor="email">Email</label>
+        </div>
+        <input type="email" name="email" id="email" placeholder="Email" />
+      </div>
+    </form>
+  </div>
 );
 
 export default SignInForm;
