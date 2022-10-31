88c656939d5ce02b351aff766e7bb2764934bcc8
unknown <abodyalex1@gmail.com>
2022-11-01 00:48:58 +0300

    Add styles to splash component

--- src/components/sign-in-sign-up/splash.js
+++ src/components/sign-in-sign-up/splash.js
@@ -1,46 +1,139 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { BsTwitter } from 'react-icons/bs';
-import { FaLinkedinIn, FaFacebookF } from 'react-icons/fa';
-import { AiOutlineGooglePlus } from 'react-icons/ai';
+// import { BsTwitter } from 'react-icons/bs';
+// import { FaLinkedinIn, FaFacebookF } from 'react-icons/fa';
+// import { AiOutlineGooglePlus } from 'react-icons/ai';
 import logo from '../../images/cars-bnb-logo.png';
 import '../../styles/signin_signup.css';
 
 const Splash = () => (
   <div>
-    <div>
-      <div className="logo d-flex justify-content-center align-items-center">
-        <img src={logo} alt="Cars bnb logo" />
+    <form>
+      <a
+        href="https://front.codes/"
+        className="logo"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={logo} alt="" />
+      </a>
+
+      <div className="section">
+        <div className="container">
+          <div className="row full-height justify-content-center">
+            <div className="col-12 text-center align-self-center py-5">
+              <div className="section pb-5 pt-5 pt-sm-2 text-center">
+                <h6 className="mb-0 pb-3">
+                  <span>Log In </span>
+                  <span>Sign Up</span>
+                </h6>
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  id="reg-log"
+                  name="reg-log"
+                />
+                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                <label htmlFor="reg-log" />
+                <div className="card-3d-wrap mx-auto">
+                  <div className="card-3d-wrapper">
+                    <div className="card-front">
+                      <div className="center-wrap">
+                        <div className="section text-center">
+                          <h4 className="mb-4 pb-3">Log In</h4>
+                          <div className="form-group">
+                            <input
+                              type="email"
+                              name="logemail"
+                              className="form-style"
+                              placeholder="Your Email"
+                              id="logemail"
+                              autoComplete="off"
+                            />
+                            <i className="input-icon uil uil-at" />
+                          </div>
+                          <div className="form-group mt-2">
+                            <input
+                              type="password"
+                              name="logpass"
+                              className="form-style"
+                              placeholder="Your Password"
+                              id="logpass"
+                              autoComplete="off"
+                            />
+                            <i className="input-icon uil uil-lock-alt" />
+                          </div>
+                          <button type="button" className="btn mt-4">
+                            submit
+                          </button>
+                          <p className="mb-0 mt-4 text-center">
+                            <a href="#0" className="link">
+                              Forgot your password?
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="card-back">
+                      <div className="center-wrap">
+                        <div className="section text-center">
+                          <h4 className="mb-4 pb-3">Sign Up</h4>
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              name="logname"
+                              className="form-style"
+                              placeholder="Your Full Name"
+                              id="logname"
+                              autoComplete="off"
+                            />
+                            <i className="input-icon uil uil-user" />
+                          </div>
+                          <div className="form-group mt-2">
+                            <input
+                              type="email"
+                              name="logemail"
+                              className="form-style"
+                              placeholder="Your Email"
+                              id="logemail"
+                              autoComplete="off"
+                            />
+                            <i className="input-icon uil uil-at" />
+                          </div>
+                          <div className="form-group mt-2">
+                            <input
+                              type="password"
+                              name="logpass"
+                              className="form-style"
+                              placeholder="Your Password"
+                              id="logpass"
+                              autoComplete="off"
+                            />
+                            <i className="input-icon uil uil-lock-alt" />
+                          </div>
+                          <button type="button" className="btn mt-4">
+                            submit
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="d-flex align-items-center flex-column">
-        <Link
-          to="/login"
-          className="mb-5 sign-btn"
-        >
-          <span>Sign In</span>
-        </Link>
-        <Link
-          to="/signup"
-          className="mb-3 sign-btn"
-        >
-          <span>Create Account</span>
-        </Link>
-      </div>
-      <div className=" d-flex socials justify-content-center">
-        <div className="d-flex justify-content-center align-items-center icons-wrap">
-          <FaFacebookF />
-        </div>
-        <div className="d-flex justify-content-center align-items-center icons-wrap">
-          <FaLinkedinIn />
-        </div>
-        <div className="d-flex justify-content-center align-items-center icons-wrap">
-          <AiOutlineGooglePlus />
-        </div>
-        <div className="d-flex justify-content-center align-items-center icons-wrap">
-          <BsTwitter />
-        </div>
-      </div>
-    </div>
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor="start">year_released</label>
+
+      <input
+        type="date"
+        id="start"
+        name="trip-start"
+        min="1990-01-01"
+        max="2022-12-31"
+      />
+    </form>
   </div>
 );
 
