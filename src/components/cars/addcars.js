10e608629044465e2cc129d6d196293690e72d51
unknown <abodyalex1@gmail.com>
2022-11-01 17:02:39 +0300

    Add mobile version for cars form

--- src/components/cars/addcars.js
+++ src/components/cars/addcars.js
@@ -1,34 +1,46 @@
 import React from 'react';
+import logo from '../../images/cars-bnb-logo.png';
 
 function Addcars() {
   return (
-    <div>
-      <form>
-        <div className="container">
-          <div className="brand-logo" />
-          <div className="brand-title">Cars_BNB</div>
+    <div className="big">
+      <div className="container">
+        <form>
+          <div className="brand-logo">
+            {' '}
+            <img src={logo} alt="Cars bnb logo" />
+            {' '}
+          </div>
+
           <div className="inputs">
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label>Model</label>
+            <label className="green">Model</label>
             <input type="text" placeholder="Model" />
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label>Brand</label>
+            <label className="green">Brand</label>
             <input type="text" placeholder="Brand" />
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="start">date</label>
+            <label className="green" htmlFor="date">
+              date
+            </label>
 
             <input
               type="date"
-              id="start"
-              name="trip-start"
+              id="date"
+              name="trip-date"
               min="1990-01-01"
               max="2022-12-31"
             />
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label className="green">car Photo</label>
+            <input type="text" placeholder="car Photo" />
 
-            <button type="submit">Add</button>
+            <button className=" mr" type="submit">
+              Add
+            </button>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
