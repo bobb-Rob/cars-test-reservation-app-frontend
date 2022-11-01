469d2c3dff986b72995130f23e789a914203efdf
shahnawaza75 <shahnawaza75@gmail.com>
2022-11-01 13:53:36 +0100

    Add carsPage compnent

---
+++ src/components/cars/CarsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Cars from './Cars';
+
+const CarsPage = () => (
+  <>
+    <nav className="navbar">Sidebar</nav>
+    <Cars />
+  </>
+);
+
+export default CarsPage;
