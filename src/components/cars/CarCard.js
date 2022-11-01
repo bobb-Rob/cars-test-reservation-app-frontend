44ad5712a053ae701cf32a7f3d950ba7bef5d5ed
shahnawaza75 <shahnawaza75@gmail.com>
2022-11-01 10:24:57 +0100

    Add car CArd component

--- src/components/cars/CarCard.js
+++ src/components/cars/CarCard.js
@@ -1,17 +1,39 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import PropType from 'prop-types';
+import { FaFacebookF } from 'react-icons/fa';
+import { BsInstagram, BsTwitter } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 
-const Cars = ({image, }) => {
+const CarCard = ({image, model, description}) => {
   
   return (
     <div>
       <div className="image-wrap">
         <img src={image} alt={model} />
       </div>
-      <h2></h2>
-      <p></p>
+      <h2>{ model }</h2>
+      <p>{description}</p>
+      <div className=" d-flex socials justify-content-center">
+        <div className="d-flex justify-content-center align-items-center icons-wrap">
+          <FaFacebookF />
+        </div>       
+        <div className="d-flex justify-content-center align-items-center icons-wrap">
+          <BsTwitter />
+        </div>
+        <div className="d-flex justify-content-center align-items-center icons-wrap">
+          <BsInstagram />
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Cars;
+CarCard.propType = {
+  image: PropType.string.isRequired,
+  model: PropType.string.isRequired,
+  description: PropType.string.isRequired,
+};
+
+export default CarCard;
+
+
