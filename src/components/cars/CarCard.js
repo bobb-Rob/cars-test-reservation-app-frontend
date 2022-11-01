2a7496a90d399d2e27e31c2ecf6dc6a43853797f
shahnawaza75 <shahnawaza75@gmail.com>
2022-11-01 13:55:14 +0100

    Add carCard compnent

--- src/components/cars/CarCard.js
+++ src/components/cars/CarCard.js
@@ -1,39 +1,33 @@
 import React from 'react';
-import PropType from 'prop-types';
+import propTypes from 'prop-types';
 import { FaFacebookF } from 'react-icons/fa';
 import { BsInstagram, BsTwitter } from 'react-icons/bs';
-import { useNavigate } from 'react-router-dom';
 
-const CarCard = ({image, model, description}) => {
-  
-  return (
-    <div>
-      <div className="image-wrap">
-        <img src={image} alt={model} />
+const CarCard = ({ image, model, description }) => (
+  <div>
+    <div className="image-wrap">
+      <img src={image} alt={model} />
+    </div>
+    <h2>{ model }</h2>
+    <p>{description}</p>
+    <div className=" d-flex socials justify-content-center">
+      <div className="d-flex justify-content-center align-items-center icons-wrap">
+        <FaFacebookF />
       </div>
-      <h2>{ model }</h2>
-      <p>{description}</p>
-      <div className=" d-flex socials justify-content-center">
-        <div className="d-flex justify-content-center align-items-center icons-wrap">
-          <FaFacebookF />
-        </div>       
-        <div className="d-flex justify-content-center align-items-center icons-wrap">
-          <BsTwitter />
-        </div>
-        <div className="d-flex justify-content-center align-items-center icons-wrap">
-          <BsInstagram />
-        </div>
+      <div className="d-flex justify-content-center align-items-center icons-wrap">
+        <BsTwitter />
+      </div>
+      <div className="d-flex justify-content-center align-items-center icons-wrap">
+        <BsInstagram />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
-CarCard.propType = {
-  image: PropType.string.isRequired,
-  model: PropType.string.isRequired,
-  description: PropType.string.isRequired,
+CarCard.propTypes = {
+  image: propTypes.string.isRequired,
+  model: propTypes.string.isRequired,
+  description: propTypes.string.isRequired,
 };
 
 export default CarCard;
-
-
