c97e4fd27226767424ac96589a003b8f7e98b4ac
shahnawaza75 <shahnawaza75@gmail.com>
2022-11-01 10:25:59 +0100

    Add carSlice.js redux reducers and actions

---
+++ src/redux/Cars/carSlice.js
@@ -0,0 +1,40 @@
+
+export const getCars = createAsyncThunk(
+  'cars/getCars',
+  async () => {
+  const token = localStorage.getItem('loginToken');
+  const url = 'http://localhost:3001/cars';
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  };
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+}
+);
+
+const carSlice = createSlice({
+  name: 'cars',
+  initialState: {
+    cars: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: {
+    [getCars.pending]: (state) => {
+      state.status = 'loading';
+    }
+    [getCars.fulfilled]: (state, action) => {
+    
+    },
+    [getCars.rejected]: (state, action) => {
+    }
+  },
+});
+
+export default carSlice.reducer;
