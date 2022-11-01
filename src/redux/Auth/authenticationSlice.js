b3c62f7ed82025006211109129f868e74d605f37
shahnawaza75 <shahnawaza75@gmail.com>
2022-11-01 18:41:19 +0100

    Change long folder name from signIn... to Auth

---
+++ src/redux/Auth/authenticationSlice.js
@@ -0,0 +1,84 @@
+import { createAsyncThunk, createSlice, current } from '@reduxjs/toolkit';
+import setJwtToken from './saveJwtToken';
+
+export const signUpUser = createAsyncThunk(
+  'authentication/signUpUser',
+  async (user, { rejectWithValue }) => {
+    try {
+      const response = await fetch('http://localhost:3001/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user }),
+      });
+      setJwtToken(response, 'signupToken');
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  },
+);
+
+export const signInUser = createAsyncThunk(
+  'authentication/signInUser',
+  async (user, { rejectWithValue }) => {
+    try {
+      const response = await fetch('http://localhost:3001/users/sign_in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user }),
+      });
+      setJwtToken(response, 'loginToken');
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  },
+);
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const authenticationSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [signUpUser.fulfilled]: (state, action) => {
+      const {
+        admin, email, id, name,
+      } = action.payload.status.data;
+      const newState = {
+        ...current(state),
+        name,
+        email,
+        id,
+        admin,
+      };
+      return newState;
+    },
+    [signInUser.fulfilled]: (state, action) => {
+      const {
+        admin, email, id, name,
+      } = action.payload.user;
+      const newState = {
+        ...current(state),
+        name,
+        email,
+        id,
+        admin,
+      };
+      return newState;
+    },
+  },
+});
+
+export default authenticationSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 /* eslint-disable-next-line import/no-extraneous-dependencies */
 import logger from 'redux-logger';
 import CarsReducer from './Cars/carSlice';
-import authenticationReducer from './SignInSignUp/authenticationSlice';
+import authenticationReducer from './Auth/authenticationSlice';
 
 export default configureStore({
   reducer: {
