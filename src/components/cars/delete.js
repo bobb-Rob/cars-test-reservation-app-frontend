48a330f7c38c42ea249c018d998059962887f32d
unknown <abodyalex1@gmail.com>
2022-11-03 01:16:27 +0300

    add delete components

--- src/components/cars/delete.js
+++ src/components/cars/delete.js
@@ -1,7 +1,82 @@
 import React from 'react';
+import '../../styles/delete.css';
+import car from '../../images/car.jpg';
 
-function Delete() {
-  return <div>Delete</div>;
+export default function Delete() {
+  return (
+    <div className='flex'>
+      <div className='card g-col-6'>
+        <img src={car} className='card-img-top' alt='...' />
+        <div className='card-body'>
+          <h5 className='card-title'>Brand and Model</h5>
+          <p className='card-text'>
+            Some quick example text to build on the card title and make up the
+            bulk of the card content.
+          </p>
+        </div>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>Date</li>
+        </ul>
+        <div className='card-body'>
+          <button type='button' className='btn btn-danger'>
+            Delete
+          </button>
+        </div>
+      </div>
+      <div className='card g-col-6'>
+        <img src={car} className='card-img-top' alt='...' />
+        <div className='card-body'>
+          <h5 className='card-title'>Brand and Model</h5>
+          <p className='card-text'>
+            Some quick example text to build on the card title and make up the
+            bulk of the card content.
+          </p>
+        </div>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>Date</li>
+        </ul>
+        <div className='card-body'>
+          <button type='button' className='btn btn-danger'>
+            Delete
+          </button>
+        </div>
+      </div>
+      <div className='card g-col-6'>
+        <img src={car} className='card-img-top' alt='...' />
+        <div className='card-body'>
+          <h5 className='card-title'>Brand and Model</h5>
+          <p className='card-text'>
+            Some quick example text to build on the card title and make up the
+            bulk of the card content.
+          </p>
+        </div>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>Date</li>
+        </ul>
+        <div className='card-body'>
+          <button type='button' className='btn btn-danger'>
+            Delete
+          </button>
+        </div>
+      </div>
+      <div className='card g-col-6'>
+        <img src={car} className='card-img-top' alt='...' />
+        <div className='card-body'>
+          <h5 className='card-title'>Brand and Model</h5>
+          <p className='card-text'>
+            Some quick example text to build on the card title and make up the
+            bulk of the card content.
+          </p>
+        </div>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>Date</li>
+        </ul>
+        <div className='card-body'>
+          <button type='button' className='btn btn-danger'>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Delete;
