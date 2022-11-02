f23c87de625b90a883e9ee344e6e65a4dde8cc45
bobb-rob <robertsonakpan@gmail.com>
2022-11-02 17:51:48 +0100

    Fix Navbar issues

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,45 +3,50 @@ import { NavLink, Link } from 'react-router-dom';
 import {
   FaFacebookF, FaTwitter, FaPinterest, FaVimeoV, FaGooglePlusG,
 } from 'react-icons/fa';
+import { HiMenu } from 'react-icons/hi';
 import '../../styles/navbar.css';
 import logo from '../../images/cars-bnb-logo.png';
 
 function Navbar() {
   return (
-    <div className="my-navbar">
-      <div>
-        <div className="logo">
-          <img src={logo} alt="carsBnb Logo" />
-        </div>
-        <div className="my-nav-links">
-          <NavLink to="/cars" className="active">CARS</NavLink>
-          <NavLink to="/addcar">ADD CAR</NavLink>
-          <NavLink to="/reserve">RESERVE</NavLink>
-          <NavLink to="/reservation-list">MY RESERVATIONS</NavLink>
-        </div>
+    <header className="my-navbar">
+      <div className="logo">
+        <img src={logo} alt="carsBnb Logo" />
       </div>
-
-      <div className="nav-footer">
-        <div className="nav-icons">
-          <Link to="/">
-            <FaTwitter />
-          </Link>
-          <Link to="/">
-            <FaFacebookF />
-          </Link>
-          <Link to="/">
-            <FaGooglePlusG />
-          </Link>
-          <Link to="/">
-            <FaVimeoV />
-          </Link>
-          <Link to="/">
-            <FaPinterest />
-          </Link>
+      <nav className="menu">
+        <div className="mobile-nav desktop-nav">
+          <div className="my-nav-links">
+            <NavLink to="/cars" className="active">CARS</NavLink>
+            <NavLink to="/addcar">ADD CAR</NavLink>
+            <NavLink to="/reserve">RESERVE</NavLink>
+            <NavLink to="/reservation-list">MY RESERVATIONS</NavLink>
+          </div>
+          <div className="nav-footer">
+            <div className="nav-icons">
+              <Link to="/">
+                <FaTwitter />
+              </Link>
+              <Link to="/">
+                <FaFacebookF />
+              </Link>
+              <Link to="/">
+                <FaGooglePlusG />
+              </Link>
+              <Link to="/">
+                <FaVimeoV />
+              </Link>
+              <Link to="/">
+                <FaPinterest />
+              </Link>
+            </div>
+            <p>2022 © All Rights Reserved</p>
+          </div>
         </div>
-        <p>2022 © All Rights Reserved</p>
-      </div>
-    </div>
+        <button type="button" className="hamburger">
+          <HiMenu />
+        </button>
+      </nav>
+    </header>
   );
 }
 
